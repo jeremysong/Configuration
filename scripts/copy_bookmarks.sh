3b3a1a08476544df4bc4f7d2a9813f79b9f394ed
Jeremy Song <yanson@amazon.com>
2022-11-22 16:59:34 -0800

    Add brave browser bookmarks

--- scripts/copy_bookmarks.sh
+++ scripts/copy_bookmarks.sh
@@ -1,3 +1,5 @@
 #1/bin/sh
 
 cp /Users/yanson/Library/Application\ Support/Google/Chrome/Default/Bookmarks* /Users/yanson/Amazon\ WorkDocs\ Drive/My\ Documents/Chrome\ Bookmarks
+
+cp /Users/yanson/Library/Application\ Support/BraveSoftware/Brave-Browser/Default/Bookmarks* /Users/yanson/Amazon\ WorkDocs\ Drive/My\ Documents/Brave\ Bookmarks
