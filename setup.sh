3e80529e823362b76a9bea9efefed92467c537cb
Jeremy Song <1678342+jeremysong@users.noreply.github.com>
2023-08-19 23:01:42 -0700

    Add more brew command lines

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@ ln -s ./Configuration/.zshrc ~/.zshrc
 ln -s ./Configuration/.vimrc ~/.vimrc
 ln -s ./Configuration/.tmux.conf ~/.tmux.conf
 
+brew install --cask corretto
+brew install --cask miniconda
+brew install --cask multipass
 brew install fortune cowsay lolcat
