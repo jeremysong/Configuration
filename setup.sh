98bf73ce4200718b8fc14d604fe040d8cfc69f59
Jeremy Song <yanson@amazon.com>
2024-01-13 12:23:15 -0800

    Rearrange stuff

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ ln -f -s ./Configuration/.zshrc ~/.zshrc
 ln -f -s ./Configuration/.vimrc ~/.vimrc
 ln -f -s ./Configuration/.tmux.conf ~/.tmux.conf
 
+brew install fortune cowsay lolcat
+brew install apache-spark
+brew install stats
+brew install nvim
 brew install --cask corretto
 brew install --cask miniconda
 brew install --cask visual-studio-code
@@ -13,8 +17,4 @@ brew install --cask rectangle
 brew install --cask 1password
 brew install --cask brave-browser
 brew install --cask github
-brew install fortune cowsay lolcat
-brew install apache-spark
-brew install stats
 brew install --cask notion
-brew install nvim
