2929dfbc918dc950abecbeec0ad1fe88a01584cf
Jeremy Song <1678342+jeremysong@users.noreply.github.com>
2024-01-13 08:34:15 -0800

    Change cursor type and font size

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 ## Configure zsh, vim, and tmux
-ln -s ./Configuration/.zshrc ~/.zshrc
-ln -s ./Configuration/.vimrc ~/.vimrc
-ln -s ./Configuration/.tmux.conf ~/.tmux.conf
+ln -f -s ./Configuration/.zshrc ~/.zshrc
+ln -f -s ./Configuration/.vimrc ~/.vimrc
+ln -f -s ./Configuration/.tmux.conf ~/.tmux.conf
 
 brew install --cask corretto
 brew install --cask miniconda
-brew install --cask multipass
+brew install --cask visual-studio-code
+brew install --cask jetbrains-toolbox
+brew install --cask rectangle
+brew install --cask 1password
+brew install --cask brave-browser
+brew install --cask github
 brew install fortune cowsay lolcat
+brew install apache-spark
+brew install stats
